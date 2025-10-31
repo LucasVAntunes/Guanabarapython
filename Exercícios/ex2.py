658ae2cd84aac41c5b23f8d151e1b40c19c72932
LucasVAntunes <lucasvieira001@gmail.com>
2025-10-31 14:16:26 +0000

    firstcommit

---
+++ Exercícios/ex2.py
@@ -0,0 +1,3 @@
+nome = input ('Qual o seu nome? ')
+
+print (f'Olá {nome} prazer em te conhecer!')
---
+++ ex3.1.py
@@ -0,0 +1,5 @@
+d = input ('Em qual dia você nasceu? ')
+m = input ('Em qual mês você nasceu? ')
+a = input ('Em qual ano você nasceu? ')
+
+print (f'Você nasceu no dia {d} de {m} de {a}')
